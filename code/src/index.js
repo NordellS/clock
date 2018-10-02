4ba06c21b1d39909951cb8928455b2bc365cd2a3
SaraNordell <sara@nordell.me>
2018-10-02 12:51:24 +0200

    Assignment done (Stretch Goals 1-3)

--- code/src/index.js
+++ code/src/index.js
@@ -4,11 +4,36 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+
   render() {
     return (
-      <div>
+      <div className="clockContainer">
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is <br/> {this.state.date.toLocaleTimeString()}</h2>
+        <div className="buttonContainer">
+          <button className="pauseButton" onClick={this.componentWillUnmount}> Pause </button>
+          <button className="runButton" onClick={this.componentDidMount}> Run </button>
+        </div>
       </div>
     )
   }
